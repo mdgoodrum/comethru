d16fec09363480477485222319772213d705a825
Thomas Spader <thomas.spader@gmail.com>
2020-04-23 13:51:34 -0400

    i am a CSS god

--- react-native/src/Components/Login.js
+++ react-native/src/Components/Login.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Button, Text, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, Image, Text, TextInput, StyleSheet } from 'react-native'
 import { MainOrange } from '../Pallet'
 
 export default class Login extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    onChangeUsername(usernameInput) {
+        this.setState({
+            username: usernameInput
+        })
+    }
+
+    onChangePassword(passwordInput) {
+        this.setState({
+            password: passwordInput
+        })
     }
 
     render() {
@@ -15,7 +32,19 @@ export default class Login extends Component {
                         <Text style={styles.signUpButtonText}>sign up!</Text>
                     </TouchableOpacity>
                 </View>
-                <Text></Text>
+                <View style={styles.logoContainer}>
+                    <Text style={styles.logo}>ComeThru</Text>
+                </View>
+                <View style={styles.imageContainer}>
+                    <Image style={styles.image} source={require('../Assets/horse.jpg')} />
+                </View>
+                <View style={styles.usernameAndPasswordContainer}>
+                    <Text>Username</Text>
+                    <Text>Password</Text>
+                    <TextInput style={styles.usernameAndPasswordEntry} onChangeText={text => this.onChangeUsername(text)}></TextInput>
+                    <TextInput onChangeText={this.onChangePassword}></TextInput>
+                </View>
+
             </View>
         )
     }
@@ -24,28 +53,74 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
     content: {
         flex: 1,
-        flexDirection: 'column'
+        flexDirection: 'column',
     },
     signUpButton: {
         backgroundColor: MainOrange,
         textAlign: 'center',
         borderRadius: 5,
-        height: '5%',
-        width: '20%',
         marginRight: '5%',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+
     },
-    signUpButtonText:{
-        color:'#fff',
+    signUpButtonText: {
+        color: '#fff',
         fontWeight: 'bold',
-        fontStyle: 'italic'
+        fontStyle: 'italic',
+        paddingLeft: '5%',
+        paddingRight: '5%'
     },
     topBar: {
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'flex-end',
-    }
+        maxHeight: '5%',
+        width: '100%',
+
+    },
+    logoContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginRight: '5%',
+        borderWidth: 1,
+        borderColor: 'red'
+    },
+    logo: {
+        fontWeight: 'bold',
+        fontSize: 32,
+        margin: "5%"
+    },
+    imageContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '25%',
+        width: '100%',
+    },
+    image: {
+        height: '100%',
+        width: '100%',
+    },
+
+    attractivePeople: {
+
+    },
+    usernameAndPasswordContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        borderColor: 'red',
+        borderWidth: 1,
+
+    },
+    usernameAndPasswordEntry: {
+        borderColor: 'red',
+        borderWidth: 1
+    },
+
 })
