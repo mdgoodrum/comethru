ea8f40ba3688bfbccbd45a9f2022d93a2f76d3d3
Michael Goodrum <michael@vlipsy.com>
2020-05-13 17:39:00 -0400

    update create address component with real functionality

--- react-native/src/Components/CreateAddress.js
+++ react-native/src/Components/CreateAddress.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet, TextInput } from 'react-native'
+import { 
+    View, 
+    TouchableOpacity, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    ToastAndroid, 
+    Platform,
+    Alert, 
+} from 'react-native'
 import { MainOrange, FooterGray } from '../Pallet'
 
 import { apiEndpoint } from '../API'
@@ -10,50 +19,95 @@ export default class Login extends Component {
         super(props)
 
         this.state = {
-           name: '',
-           address: '',
-           twenty_one: false
+           streetNumber: null,
+           streetName: null,
+           streetType: null,
+           streetDirection: null,
+           addressType: '',
+           addressTypeId: '',
+           majorMunicipality: null,
+           governingDistrict: null,
+           postalArea: null,
+           country: 'USA',
         }
     }
 
     onPress() {
-        const body = {
-            "name": this.state.name,
-            "address": 'http://localhost:8000/api/models/addresses/1/', // placeholder for now till we decide how we wanna do this
-            "twenty_one": this.state.twenty_one
-        };
-        axios
-            .post(apiEndpoint('/models/venues/'), body)
-            .then(() => {
-                this.props.navigation.navigate('Events')
-            })
-            .catch(error => {
-                console.log(error)
-            });
-    }
-
-    onChangeName(name) {
-        this.setState({
-            name
-        })
-    }
-
-    onChangeAddress(address) {
-        this.setState({
-            address
-        })
-    }
-
-    onChangeTwentyOne(input) {
-        if (input === '') {
-            this.setState({
-                twenty_one: false
-            })
+        if (!this.state.streetNumber || 
+            !this.state.streetName || 
+            !this.state.majorMunicipality || 
+            !this.state.governingDistrict || 
+            !this.state.postalArea) {
+            const msg = 'Must fill out Street Address, City, State and Zip.';
+            if (Platform.OS === 'android') {
+                ToastAndroid.show(msg, ToastAndroid.SHORT)
+            } else {
+                Alert.alert(msg);
+            }
         } else {
-            this.setState({
-                twenty_one: true
-            })
+            const body = {
+                street_number: this.state.streetNumber,
+                street_name: this.state.streetName,
+                street_type: this.state.streetType,
+                street_direction: this.state.streetDirection,
+                address_type: this.state.addressType,
+                address_type_id: this.state.addressTypeId,
+                major_municipality: this.state.majorMunicipality,
+                governing_district: this.state.governingDistrict,
+                postal_area: this.state.postalArea,
+                country: this.state.country,
+            };
+            axios
+                .post(apiEndpoint('/models/addresses/'), body)
+                .then(() => {
+                    this.props.navigation.navigate('Events')
+                })
+                .catch(error => {
+                    console.log(error)
+                });
         }
+    }
+
+    onChangeStreetAddress(address) {
+        const values = address.split(' ');
+        const streetNumber = values[0] || '';
+        const streetName = values[1] || '';
+        const streetType = values[2] || '';
+        const streetDirection = values[3] ? values[3] : '';
+        this.setState({
+            streetNumber,
+            streetName,
+            streetType,
+            streetDirection,
+        })
+    }
+
+    onChangeStreetAddress2(address2) {
+        const values = address2.split(' ');
+        const addressType = values[0] || '';
+        const addressTypeId = values[1] || '';
+        this.setState({
+            addressType,
+            addressTypeId,
+        })
+    }
+
+    onChangeCity(majorMunicipality) {
+        this.setState({
+            majorMunicipality
+        })
+    }
+
+    onChangeState(governingDistrict) {
+        this.setState({
+            governingDistrict
+        })
+    }
+
+    onChangePostalZip(postalArea) {
+        this.setState({
+            postalArea
+        })
     }
 
     cancel() {
@@ -64,34 +118,44 @@ export default class Login extends Component {
         return (
             <View style={styles.content}>
                 <View style={styles.usernameAndPasswordContainer}>
-                    <Text style={styles.usernameAndPasswordText}>Venue Name</Text>
+                    <Text style={styles.usernameAndPasswordText}>Street Address</Text>
                     <TextInput
                         style={styles.usernameAndPasswordEntry}
                         autoCapitalize={"none"}
-                        onChangeText={text => this.onChangeName(text)} />
-                    <Text style={styles.usernameAndPasswordText}>Venue Address</Text>
+                        onChangeText={text => this.onChangeStreetAddress(text)} />
+                    <Text style={styles.usernameAndPasswordText}>Street Address Line 2</Text>
                     <TextInput
                         style={styles.usernameAndPasswordEntry}
                         autoCapitalize={"none"}
-                        onChangeText={text => this.onChangeAddress(text)} />
-                    <Text style={styles.usernameAndPasswordText}>21+</Text>
+                        onChangeText={text => this.onChangeStreetAddress2(text)} />
+                    <Text style={styles.usernameAndPasswordText}>City</Text>
                     <TextInput
                         style={styles.usernameAndPasswordEntry}
                         autoCapitalize={"none"}
-                        onChangeText={text => this.onChangeTwentyOne(text)} />
+                        onChangeText={text => this.onChangeCity(text)} />
+                    <Text style={styles.usernameAndPasswordText}>State</Text>
+                    <TextInput
+                        style={styles.usernameAndPasswordEntry}
+                        autoCapitalize={"none"}
+                        onChangeText={text => this.onChangeState(text)} />
+                    <Text style={styles.usernameAndPasswordText}>Postal / Zip Code</Text>
+                    <TextInput
+                        style={styles.usernameAndPasswordEntry}
+                        autoCapitalize={"none"}
+                        onChangeText={text => this.onChangePostalZip(text)} />
                 </View>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity   
                         style={styles.myButton}
                         onPress={() => this.cancel()}
                         >
-                        <Text style={styles.createEventButtonText}>Cancel</Text>
+                        <Text style={styles.buttonText}>Cancel</Text>
                     </TouchableOpacity>
                     <TouchableOpacity   
                         style={styles.myButton}
                         onPress={() => this.onPress()}
                         >
-                        <Text style={styles.createEventButtonText}>Create Venue</Text>
+                        <Text style={styles.buttonText}>Create Address</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -104,6 +168,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
     myButton: {
         height: 80,
         width: 80,
@@ -111,7 +179,7 @@ const styles = StyleSheet.create({
         backgroundColor: MainOrange,
         justifyContent: 'center',
     },
-    createEventButtonText: {
+    buttonText: {
         color: '#fff',
         textAlign: 'center',
         fontWeight: 'bold',
@@ -121,29 +189,10 @@ const styles = StyleSheet.create({
         borderColor: MainOrange,
         borderWidth: 1,
         borderRadius: 5,
-        width: '60%',
-        height: '15%',
-        marginBottom: '5%',
+        width: '90%',
+        height: '8%',
+        marginBottom: '2%',
         textAlign: 'center'
-    },
-    signUpButton: {
-        backgroundColor: MainOrange,
-        textAlign: 'center',
-        borderRadius: 5,
-        marginRight: '5%',
-        height: '75%',
-        minHeight: '75%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    signUpButtonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-        paddingLeft: '2%',
-        paddingRight: '2%'
     },
     usernameAndPasswordText: {
         color: MainOrange,
