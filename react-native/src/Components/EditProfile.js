b9c260c9fcabc0c228fa57aaef4dfe6f4601e58a
Michael Goodrum <michael@vlipsy.com>
2020-05-16 14:17:05 -0400

    swap placeholder edit profile for real functionality

--- react-native/src/Components/EditProfile.js
+++ react-native/src/Components/EditProfile.js
@@ -1,102 +1,89 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { View, TouchableOpacity, Text, StyleSheet, TextInput } from 'react-native'
 import { MainOrange, FooterGray } from '../Pallet'
 
 import { apiEndpoint } from '../API'
 import axios from 'axios'
+import { useSelector } from 'react-redux'
 
-export default class Login extends Component {
-    constructor(props) {
-        super(props)
+export default EditProfile = (props) => {
+    const user = useSelector(state => state.loggedInUser)
 
-        this.state = {
-           name: '',
-           address: '',
-           twenty_one: false
-        }
-    }
+    const [firstName, setFirstName] = useState(user.first_name)
+    const [lastName, setLastName] = useState(user.last_name)
+    const [email, setEmail] = useState(user.email)
 
-    onPress() {
+    onPress = () => {
         const body = {
-            "name": this.state.name,
-            "address": 'http://localhost:8000/api/models/addresses/1/', // placeholder for now till we decide how we wanna do this
-            "twenty_one": this.state.twenty_one
+            "first_name": firstName,
+            "last_name": lastName,
+            "email": email
         };
+        console.log(body)
         axios
-            .post(apiEndpoint('/models/venues/'), body)
+            .patch(apiEndpoint(`/models/users/${user.id}/`), body)
             .then(() => {
-                this.props.navigation.navigate('Events')
+                props.navigation.navigate('Events')
             })
             .catch(error => {
                 console.log(error)
             });
     }
 
-    onChangeName(name) {
-        this.setState({
-            name
-        })
+    onChangeFirstName = (firstName) => {
+        setFirstName(firstName)
     }
 
-    onChangeAddress(address) {
-        this.setState({
-            address
-        })
+    onChangeLastName = (lastName) => {
+        setLastName(lastName)
     }
 
-    onChangeTwentyOne(input) {
-        if (input === '') {
-            this.setState({
-                twenty_one: false
-            })
-        } else {
-            this.setState({
-                twenty_one: true
-            })
-        }
+    onChangeEmail = (email) => {
+        setEmail(email)
     }
 
-    cancel() {
-        this.props.navigation.navigate('Events')
+    cancel = () => {
+        props.navigation.navigate('Events')
     }
 
-    render() {
-        return (
-            <View style={styles.content}>
-                <View style={styles.usernameAndPasswordContainer}>
-                    <Text style={styles.usernameAndPasswordText}>Venue Name</Text>
-                    <TextInput
-                        style={styles.usernameAndPasswordEntry}
-                        autoCapitalize={"none"}
-                        onChangeText={text => this.onChangeName(text)} />
-                    <Text style={styles.usernameAndPasswordText}>Venue Address</Text>
-                    <TextInput
-                        style={styles.usernameAndPasswordEntry}
-                        autoCapitalize={"none"}
-                        onChangeText={text => this.onChangeAddress(text)} />
-                    <Text style={styles.usernameAndPasswordText}>21+</Text>
-                    <TextInput
-                        style={styles.usernameAndPasswordEntry}
-                        autoCapitalize={"none"}
-                        onChangeText={text => this.onChangeTwentyOne(text)} />
-                </View>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity   
-                        style={styles.myButton}
-                        onPress={() => this.cancel()}
-                        >
-                        <Text style={styles.createEventButtonText}>Cancel</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity   
-                        style={styles.myButton}
-                        onPress={() => this.onPress()}
-                        >
-                        <Text style={styles.createEventButtonText}>Create Venue</Text>
-                    </TouchableOpacity>
-                </View>
+    return (
+        <View style={styles.content}>
+            <View style={styles.usernameAndPasswordContainer}>
+                <Text style={styles.usernameAndPasswordText}>First Name</Text>
+                <TextInput
+                    style={styles.usernameAndPasswordEntry}
+                    autoCapitalize={"none"}
+                    placeholder={firstName}
+                    onChangeText={text => this.onChangeFirstName(text)} />
+                <Text style={styles.usernameAndPasswordText}>Last Name</Text>
+                <TextInput
+                    style={styles.usernameAndPasswordEntry}
+                    autoCapitalize={"none"}
+                    placeholder={lastName}
+                    onChangeText={text => this.onChangeLastName(text)} />
+                <Text style={styles.usernameAndPasswordText}>Email</Text>
+                <TextInput
+                    style={styles.usernameAndPasswordEntry}
+                    autoCapitalize={"none"}
+                    placeholder={email}
+                    onChangeText={text => this.onChangeEmail(text)} />
             </View>
-        )
-    }
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity   
+                    style={styles.myButton}
+                    onPress={() => this.cancel()}
+                    >
+                    <Text style={styles.createEventButtonText}>Cancel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity   
+                    style={styles.myButton}
+                    onPress={() => this.onPress()}
+                    >
+                    <Text style={styles.createEventButtonText}>Save</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
